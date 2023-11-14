881ef8a45a74d71f015222cba53f03b9533bd677
Tony Fialho <33699153+arachnea@users.noreply.github.com>
2023-11-14 04:48:39 +0000

    Update test.js

--- test.js
+++ test.js
@@ -9,17 +9,14 @@ describe('Button Click Test', function() {
   });
 
   it('should click the "Overwatch 2" button', async function() {
-    await driver.get('http://localhost:4200'); // replace 'your-app-port' with your app's port
+    await driver.get('http://localhost:4200');
 
-    // Locate the button based on its text content and surrounding structure
     const button = await driver.findElement(
       By.xpath("//mat-card[contains(mat-card-content, 'Overwatch 2')]//button[contains(@class, 'overlay-button')]")
     );
 
-    // Click the button
     await button.click();
 
-    // Add assertions or further actions if needed
   });
 
   after(async function() {
